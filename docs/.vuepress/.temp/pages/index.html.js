d9200cd9b4423e1889d1fec45453907a302f4cde
YIHengya <iheng13606@gmail.com>
2024-09-03 13:01:16 +0800

    feat: 打包

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,16 +1,3 @@
 import comp from "D:/project/nuxtp/vcamxdoc/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"Home\",\"lang\":\"en-US\",\"frontmatter\":{\"home\":true,\"title\":\"Home\",\"heroImage\":\"https://shop.vcamx.com/favicon.ico\",\"actions\":[{\"text\":\"开始\",\"link\":\"/get-started.html\",\"type\":\"primary\"}],\"footer\":\"MIT Licensed | Copyright © 2024-present 无界直播 Community\"},\"headers\":[],\"git\":{},\"filePathRelative\":\"README.md\"}")
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"Home\",\"lang\":\"en-US\",\"frontmatter\":{\"home\":true,\"title\":\"Home\",\"heroImage\":\"https://shop.vcamx.com/favicon.ico\",\"actions\":[{\"text\":\"开始\",\"link\":\"/get-started.html\",\"type\":\"primary\"}],\"footer\":\"MIT Licensed | Copyright © 2024-present 无界直播 Community\"},\"headers\":[],\"git\":{\"updatedTime\":1725339572000,\"contributors\":[{\"name\":\"YIHengya\",\"email\":\"iheng13606@gmail.com\",\"commits\":1}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
